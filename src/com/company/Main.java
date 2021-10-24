cb10ca7ce1bb25c46f886e6dedf2c61ff1f96d85
mili81 <milisenta81@gmail.com>
2021-10-24 22:36:28 +0300

    updated String 1st Assignment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,85 +7,81 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-	String [] array ={"Computer", "Plate", "Chair", "Girl", "Boy", "Cat", "Dog", "Shirt", "Determination"};
-                startC(array);
-                endsE(array);
-                lengthFive(array);
-                consistE(array);
-                subStringTe(array);
-                histogram(array);
+        String[] array = {"Computer", "Plate", "Chair", "Girl", "Boy", "Cat", "Dog", "Shirt", "Determination"};
+        startC(array);
+        endsE(array);
+        lengthFive(array);
+        consistE(array);
+        subStringTe(array);
+        histogram(array);
 
     }
-    public static void startC (String [] array) {
+
+    public static void startC(String[] array) {
         int count = 0;
-        for(int i=0; i< array.length; i++)
+        for (int i = 0; i < array.length; i++)
             if (array[i].charAt(0) == 'C')
                 count++;
         System.out.printf(" %s words start with C;", count);
 
 
     }
-    public static void endsE (String [] array){
+
+    public static void endsE(String[] array) {
         int count = 0;
-        for(int i=0; i< array.length; i++)
-            if(array[i].endsWith("e"))
+        for (int i = 0; i < array.length; i++)
+            if (array[i].endsWith("e"))
                 count++;
         System.out.println();
         System.out.printf(" %s words ends with e;", count);
 
     }
-    public static void lengthFive (String [] array){
+
+    public static void lengthFive(String[] array) {
         int count = 0;
-        for(int i=0; i< array.length; i++)
-            if(array[i].length() ==5)
+        for (int i = 0; i < array.length; i++)
+            if (array[i].length() == 5)
                 count++;
         System.out.println();
         System.out.printf(" %s words has length 5;", count);
     }
-    public static void consistE(String [] array){
+
+    public static void consistE(String[] array) {
         int count = 0;
-        for(int i=0; i< array.length; i++)
-            if(array[i].contains("e"))
+        for (int i = 0; i < array.length; i++)
+            if (array[i].contains("e"))
                 count++;
         System.out.println();
-        System.out.printf(" %s words has letter e;",count);
+        System.out.printf(" %s words has letter e;", count);
         System.out.println();
 
     }
-    public static void subStringTe(String[] array){
+
+    public static void subStringTe(String[] array) {
         System.out.println("Is there any element which consists of subString 'te'?");
         int count = 0;
         for (String te : array) {
             if (te.contains("te"))
                 count++;
         }
-        if(count>0) {
+        if (count > 0) {
             System.out.printf(" -Yes, there is %s words which contains subString 'te'", count);
         } else System.out.println("No, there is not.");
 
     }
 
-    //Histograms allow us to maintain the count of different categories of values.
-    //public static void histogram(String[]array){
-     //   int [] wordsLength = new int[array.length];
-     //   Arrays.setAll(wordsLength, i->array[i].length());
-    public static void histogram (String [] array) {
-        int wordsLength = 0;
-        int value = 0;
-        while (wordsLength <= array.length) {
-            int count = 0;
-            for (String word : array) {
-                int stringLength = word.length();
-                if (stringLength == wordsLength) {
-                    count++;
-                    System.out.println(count + "words have" + wordsLength + "letters");
-                } else
-                    System.out.println("something is wrong");
-
-
-// counter has 0 for all words , and wordsLength = 0 letters.. Do not understand, what is wrong
-            }
+    public static void histogram(String[] array) {
+        int[] counter = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < array.length; i++) {
+            counter[array[i].length() - 1]++;
+            System.out.println("");
         }
+        for (int i = 0; i < counter.length; i++)
+            if (counter[i] > 0) {
+                System.out.println("There are " + (i + 1) + " letters : in " + counter[i] + " word(s) in this array");
+                // 1.count of letters, 2. count of words
+            }
+    }
+}
 
 
-} //I took out 1 } just to stop printing endless code.
